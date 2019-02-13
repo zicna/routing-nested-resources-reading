5c58c51eb1a5be28569d828d6125c7a9f75ef58f
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 17:38:55 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-
-  resources :authors, only: [:show]
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
-  root 'posts#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
