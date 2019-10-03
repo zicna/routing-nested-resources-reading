93fb7536876c0a450cc12282882a6fbdfc1dd2e6
Mohawk Greene <kellyecodes@gmail.com>
2019-10-03 09:48:14 -0400

    Adds solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
   root 'posts#index'
-
 end
